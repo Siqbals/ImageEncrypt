2cdf741d6b19594d84f381cd73d243122772d0df
Saadman <doj513@usask.ca>
2025-07-14 02:36:26 -0600

    created the encrypt file func

--- encryptionfunc.py
+++ encryptionfunc.py
@@ -2,38 +2,37 @@ from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 from cryptography.hazmat.primitives import padding as sympadding
 from cryptography.hazmat.backends import default_backend
 from Irisencrypt import *
-import hashlib, os
+import os
 
-def derive_key_from_iris_signature(iris_hash: str) -> bytes:
-    return bytes.fromhex(iris_hash)[:32]  # 32-byte AES key
-
-def encrypt_file_in_place(file_path, iris_hash):
-    key = derive_key_from_iris_signature(iris_hash)
-    iv = os.urandom(16)
+def encrypt_file(file_path, iris_hash):
+    key = bytes.fromhex(iris_hash)[:32]
+    ran16 = os.urandom(16)
 
     # Read original file content
     with open(file_path, 'rb') as f:
         data = f.read()
 
-    # Pad content
-    padder = sympadding.PKCS7(128).padder()
-    padded_data = padder.update(data) + padder.finalize()
+    # add padding so AES works every time 
+    padgen = sympadding.PKCS7(128).padder()
+    repaddata = padgen.update(data) + padgen.finalize()
 
-    # Encrypt
-    cipher = Cipher(algorithms.AES(key), modes.CBC(iv), backend=default_backend())
+    # Encrypt generation
+    cipher = Cipher(algorithms.AES(key), modes.CBC(ran16), backend=default_backend())
     encryptor = cipher.encryptor()
-    ciphertext = encryptor.update(padded_data) + encryptor.finalize()
+    ciphertext = encryptor.update(repaddata) + encryptor.finalize()
 
-    # Overwrite file: store IV + ciphertext
+    #apply encryption to the file
     with open(file_path, 'wb') as f:
-        f.write(iv + ciphertext)
+        f.write(ran16 + ciphertext)
 
-    print(f"[+] File encrypted in-place: {file_path}")
+    print("encryption successful")
 
-def decrypt_file_in_place(file_path, iris_hash):
-    key = derive_key_from_iris_signature(iris_hash)
+def decrypt_file(file_path, iris_hash):
 
-    # Read IV + encrypted content
+    #use the same iris hash to get the stuff back
+    key = bytes.fromhex(iris_hash)[:32]
+
+    #get the ran16 and ciphertext
     with open(file_path, 'rb') as f:
         iv = f.read(16)
         ciphertext = f.read()
@@ -43,22 +42,20 @@ def decrypt_file_in_place(file_path, iris_hash):
     decryptor = cipher.decryptor()
     padded_data = decryptor.update(ciphertext) + decryptor.finalize()
 
-    # Remove padding
+    #removing padding 
     unpadder = sympadding.PKCS7(128).unpadder()
-    data = unpadder.update(padded_data) + unpadder.finalize()
+    unpaddata = unpadder.update(padded_data) + unpadder.finalize()
 
-    # Overwrite file with plaintext
+    #replace file content with unecrypted versions 
     with open(file_path, 'wb') as f:
-        f.write(data)
+        f.write(unpaddata)
 
-    print(f"[+] File decrypted in-place: {file_path}")
+    print("decryption successful")
 
-    iris_hash = iris_signature_from_image("iristest.jpg")
-
-iris_hash = iris_signature_from_image("iristest.jpg")
+iris_hash = irissig("iristest.jpg")
 
 # Encrypt in-place
-decrypt_file_in_place("r.txt", iris_hash)
+decrypt_file("r.txt", iris_hash)
 
 
 
