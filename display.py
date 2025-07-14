313fd91661e710ba4a24a9e20b19272793cbcead
Saadman <doj513@usask.ca>
2025-07-14 03:07:25 -0600

    finished up the first menu screen, and started the encrypt menu screen

--- display.py
+++ display.py
@@ -1,14 +1,24 @@
-from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QVBoxLayout
+from PyQt5.QtWidgets import (
+    QApplication, QWidget, QLabel, QPushButton, QVBoxLayout, QHBoxLayout, QStackedLayout
+)
 from PyQt5.QtCore import Qt
 
 
 app = QApplication([])
+
 
 #the window itself 
 window = QWidget()
 window.setWindowTitle("Iris Encrypt v1")
 window.setFixedSize(800, 500) 
 window.setStyleSheet("background-color: black;")
+
+#all menu container 
+allmenu = QStackedLayout()
+
+'''Window 1, the encrypt and decrypt menu'''
+#main page layout 
+homemenu = QVBoxLayout()
 
 #iris text label 
 title = QLabel("Iris Encrypt v1")
@@ -21,12 +31,80 @@ desc = QLabel("Welcome to Iris Encrypt v1, a place to securly encrypt any file u
 desc.setStyleSheet("color: white; font-size: 14px;")
 desc.setAlignment(Qt.AlignLeft)
 
-#label positioning 
-layout = QVBoxLayout()
-layout.addWidget(title)
-layout.addWidget(desc)
-layout.addStretch()  # pushes content to top
+#encrypt btn 
+encryptbtn = QPushButton("Encrypt")
+encryptbtn.setStyleSheet("background-color: green; color: white; font-weight: bold;")
+encryptbtn.setFixedHeight(40)
 
-window.setLayout(layout)
+#decrypt btn
+decryptbtn = QPushButton("Decrypt")
+decryptbtn.setStyleSheet("background-color: darkred; color: white; font-weight: bold;")
+decryptbtn.setFixedHeight(40)
+
+#layout container for the btns 
+btncont = QHBoxLayout()
+btncont.addWidget(encryptbtn)
+btncont.addWidget(decryptbtn)
+
+#add everything to the window 
+homemenu = QVBoxLayout()
+homemenu.addWidget(title)
+homemenu.addWidget(desc)
+homemenu.addSpacing(20)
+homemenu.addLayout(btncont)
+homemenu.addStretch()
+
+
+'''encrypt menu'''
+encryptmenu = QVBoxLayout()
+
+#encrypt title text
+encrypttitle = QLabel("Encryption Panel")
+encrypttitle.setStyleSheet("color: lime; font-size: 20px; font-weight: bold;")
+encrypttitle.setAlignment(Qt.AlignLeft)
+
+#description
+encryptdesc = QLabel("To get started, drag and drop your desired file on the Left \n then drag and drop your image of your iris on the right ")
+encryptdesc.setStyleSheet("color: white; font-size: 14px;")
+encryptdesc.setAlignment(Qt.AlignLeft)
+
+#the back button
+backbtn = QPushButton("Back")
+backbtn.setStyleSheet("background-color: gray; color: white; font-weight: bold;")
+
+#add the things to the encrypt menu
+encryptmenu.addWidget(encrypttitle)
+encryptmenu.addWidget(encryptdesc)
+encryptmenu.addStretch()
+encryptmenu.addWidget(backbtn)
+
+#first drag and drop box for the Iris 
+
+
+
+
+
+
+
+#add the entire menu to the container 
+encryptcont = QWidget()
+encryptcont.setLayout(encryptmenu)
+
+
+
+
+
+
+
+#final window settings 
+homecont = QWidget()
+homecont.setLayout(homemenu)
+allmenu.addWidget(homecont)
+allmenu.addWidget(encryptcont)
+
+encryptbtn.clicked.connect(lambda: allmenu.setCurrentIndex(1))
+backbtn.clicked.connect(lambda: allmenu.setCurrentIndex(0))
+
+window.setLayout(allmenu)
 window.show()
 app.exec_()
